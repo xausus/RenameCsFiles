99d5179f6ffa405de11b0a0c36de17bcbc89d016
Amin Bahrami <xauses@gmail.com>
2021-01-02 23:26:01 +0330

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace RenameCsFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.AllowDrop = true;
+            this.listBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 15;
+            this.listBox.Location = new System.Drawing.Point(12, 10);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(264, 259);
+            this.listBox.TabIndex = 0;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
+            this.listBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox.DragEnter += new System.Windows.Forms.DragEventHandler(this.dragEnter);
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Location = new System.Drawing.Point(14, 270);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(261, 47);
+            this.panel1.TabIndex = 1;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.dragEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 326);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox);
+            this.Name = "Form1";
+            this.Text = "Rename CS To Class";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RenameCsFiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            listBox.Items.Clear();
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop); 
+            foreach (var ww in files)
+                Process(ww);
+        }
+
+        void Process(string filename)
+        {
+            if(Path.GetExtension(filename).ToLower()!=".cs") return;
+            var n = getclassname(filename); 
+            if(n!="") rename(filename,n);
+            else
+
+            listBox.Items.Add($"{Path.GetFileName(filename)} ******");
+        }
+        void NameCleaner(string fullpath)
+
+        {
+            var ext = Path.GetExtension(fullpath);
+            var name = Path.GetFileNameWithoutExtension(fullpath);  
+            var path = Path.GetDirectoryName(fullpath);
+            var match= CleanNameR.Match(name);
+            if (match.Success)
+                name = match.Groups[1].Value; 
+            var newname = Path.Combine(path, name + ext);
+            listBox.Items.Add(name+ext);
+            File.Move(fullpath, newname);
+
+          
+        }
+
+        static readonly Regex CleanNameR=new Regex("(\\w+)(_?\\(?\\d+\\)?)$", RegexOptions.RightToLeft);
+        string getclassname(string filename)
+         {
+             string s = "";
+            // Read file using StreamReader. Reads file line by line  
+            using (StreamReader file = new StreamReader(filename))
+            {
+               
+                string ln; 
+                while ((ln = file.ReadLine()) != null)
+                {
+                    var w = ClassPat.Match(ln);
+                    if(!w.Success)  continue;
+                    s = w.Groups["name"].Value; 
+                    break;
+                } 
+                file.Close(); 
+            }
+              return s; 
+        }
+
+        static Regex ClassPat=new Regex("class (?<name>\\w+)");
+
+        void rename(string fullpath, string classname)
+        {
+            var ext = Path.GetExtension(fullpath);
+            var name = Path.GetFileNameWithoutExtension(fullpath); 
+            if (name.Length > 10) name = name.Substring(0, 5);
+            var path = Path.GetDirectoryName(fullpath);  
+                var newname =    Path.Combine(path, classname + ext);
+                if (File.Exists(newname))
+                {
+                    var number = 0; 
+            do
+            {   
+                number++;
+                newname =Path.Combine(path,$"{classname}_{number:00}{ext}"); 
+            }
+
+            while (File.Exists(newname));  
+
+                }  
+           
+            listBox.Items.Add($"{name} -> {newname.Substring(path.Length+1)}");
+            File.Move(fullpath,newname);   
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            listBox.Items.Clear();
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (var ww in files)
+                NameCleaner(ww);
+        }
+
+      
+    }
+}
